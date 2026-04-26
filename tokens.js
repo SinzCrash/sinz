45685af991a740c7d2910cba21dfc17af907997d
SinzCrash <aripgamin777@gmail.com>
2026-04-26 23:41:18 +0700

    save token

--- tokens.js
+++ tokens.js
@@ -1,6 +1,6 @@
 module.exports = {
-  token: "kicaww",
-  id: "kicaww",
+  token: "8610040575:AAEM7HvJacRAu490TWinCHueSfK6Hsu5En4",
+  id: "8610040575",
   owner: "Xhin",
-  created: "2026-04-26T10:59:11.533Z"
+  created: "2026-04-26T16:41:17.355Z"
 };
