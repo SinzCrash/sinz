ccc8491aba33f93715a91e060937a34f48743395
SinzCrash <aripgamin777@gmail.com>
2026-04-26 23:47:03 +0700

    save token

--- tokens.js
+++ tokens.js
@@ -1,6 +1,6 @@
 module.exports = {
-  token: "8610040575:AAEM7HvJacRAu490TWinCHueSfK6Hsu5En4",
-  id: "8610040575",
+  token: "8736777545:AAHhIN508SxJICUwGz_cq9XOJfy9-yUByIg",
+  id: "8736777545",
   owner: "Xhin",
-  created: "2026-04-26T16:41:17.355Z"
+  created: "2026-04-26T16:47:02.052Z"
 };
