548028892b040b16044b177e20e63316cac58955
SinzCrash <aripgamin777@gmail.com>
2026-04-27 10:22:35 +0700

    save token

--- tokens.js
+++ tokens.js
@@ -1,6 +1,6 @@
 module.exports = {
-  token: "8702337094:AAENrH3l_bA5fJ6rw98FKphXMSZfrMUAnCY",
+  token: "8702337094:AAECKvNz1g_XkNY9yv0nWFzcfquJNJNNGo0",
   id: "8702337094",
   owner: "Xhin",
-  created: "2026-04-27T03:16:02.548Z"
+  created: "2026-04-27T03:22:33.920Z"
 };
